996617e11bc901353088fdbb045345daca085d2e
Garima Negi <garimanegi60@gmail.com>
2019-05-16 12:31:43 +0530

    changes in 2

--- 2.js
+++ 2.js
@@ -1,3 +1,14 @@
-var expression = 7 + (6 * 5 + 6) / 2 - 3;
+/*var expression = 7 + (6 * 5 + 6) / 2 - 3;
 prompt("enter your expression");
 console.log("the answer is " + expression);
+*/
+
+var fan={flaps:4,
+    speed:50,
+    brand:havels,
+    switchOn:function(){
+    console.log("fan is moving");
+    }
+
+
+}
