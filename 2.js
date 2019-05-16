2cb379509139c5735b1e642612e1c80ec2e54070
Garima Negi <garimanegi60@gmail.com>
2019-05-16 15:52:09 +0530

    CHANGES IN 2

--- 2.js
+++ 2.js
@@ -1,14 +1,29 @@
 /*var expression = 7 + (6 * 5 + 6) / 2 - 3;
 prompt("enter your expression");
 console.log("the answer is " + expression);
-*/
+
 
 var fan={flaps:4,
     speed:50,
-    brand:havels,
+    brand:"havels",
     switchOn:function(){
     console.log("fan is moving");
     }
-
-
 }
+fan.flaps=3;
+console.log(fan.flaps)
+var buttonClick=document.querySelector("#button");
+//console.log(buttonClick);
+buttonClick.addEventListener("mouseover", myFunction);
+function myFunction(event) {
+    alert ("hi everyone")
+    console.log(event);
+  }
+
+  */
+
+function MyFunction()
+{
+    var a;
+}
+console.log(a);
